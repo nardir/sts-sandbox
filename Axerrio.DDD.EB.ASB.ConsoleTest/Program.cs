064ac6deeb4e15b86f57f064981c839589fd75ce
Nardi Rens <nardir@axerrio.com>
2017-11-24 17:06:24 +0100

    Azure Service Bus - Test

---
+++ Axerrio.DDD.EB.ASB.ConsoleTest/Program.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Axerrio.DDD.EB.ASB.ConsoleTest
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://sts-sandbox.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=okAS5ZUVaWkRQaoF8u6e8m7ulHZ+cZgu7nB0suhVh+M=";
+        const string TopicName = "sts-topictest";
+        const string SubscriptionName = "sts-ordering";
+
+        static ITopicClient topicClient;
+        static ISubscriptionClient subscriptionClient;
+
+        static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+
+        static async Task MainAsync(string[] args)
+        {
+            const int numberOfMessages = 10;
+
+            var csb = new ServiceBusConnectionStringBuilder(ServiceBusConnectionString);
+            csb.EntityPath = TopicName;
+
+            topicClient = new TopicClient(csb, RetryPolicy.Default);
+            //topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press any key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Register Subscription's MessageHandler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            // Send Messages
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await subscriptionClient.CloseAsync();
+            await topicClient.CloseAsync();
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the subscriptionClient is opened in ReceiveMode.PeekLock mode (which is default).
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        // Use this Handler to look at the exceptions received on the MessagePump
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+
+            return Task.CompletedTask;
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            for (var i = 0; i < numberOfMessagesToSend; i++)
+            {
+                try
+                {
+                    // Create a new message to send to the topic
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the topic
+                    await topicClient.SendAsync(message);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+                }
+            }
+        }
+    }
+}
