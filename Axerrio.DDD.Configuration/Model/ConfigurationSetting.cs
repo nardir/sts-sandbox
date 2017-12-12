4823fb7dacb3969b4f3b3b4d527cb2a2053aeac2
Nardi Rens <nardir@axerrio.com>
2017-12-12 18:39:28 +0100

    Settings

---
+++ Axerrio.DDD.Configuration/Model/ConfigurationSetting.cs
@@ -0,0 +1,63 @@
+﻿using Axerrio.DDD.BuildingBlocks;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Axerrio.DDD.Configuration.Model
+{
+    public class ConfigurationSetting: Entity<int>
+    {
+        public ConfigurationSection Section { get; private set; }
+
+        public string Key { get; private set; }
+
+        private object _value;
+
+        public string JsonValue { get; private set; }
+
+        public virtual string ValueType { get; private set; }
+
+        protected ConfigurationSetting()
+        {
+        }
+
+        public ConfigurationSetting(ConfigurationSection section, string key)
+        {
+            Section = section;
+            Key = key;
+        }
+
+        public virtual T GetValue<T>()
+        {
+            return (T)Convert.ChangeType(GetValue(typeof(T)), typeof(T));
+        }
+
+        public virtual object GetValue(Type type)
+        {
+            if ((_value == null) && !string.IsNullOrEmpty(JsonValue))
+            {
+                _value = JsonConvert.DeserializeObject(JsonValue, type);
+            }
+            return _value;
+        }
+
+        public virtual void SetValue(object value, Type type)
+        {
+            if (_value != value)
+            {
+                _value = value;
+                JsonValue = null;
+                if (value != null)
+                {
+                    JsonValue = JsonConvert.SerializeObject(value);
+                }
+            }
+
+            //var name = value.GetType().AssemblyQualifiedName.Split(Convert.ToChar(","));
+            var name = type.AssemblyQualifiedName.Split(Convert.ToChar(","));
+            ValueType = $"{name[0].Trim()}, {name[1].Trim()}";
+        }
+    }
+}
---
+++ Axerrio.DDD.Configuration/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Axerrio.DDD.Configuration.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Axerrio.DDD.Configuration
+{
+    //https://github.com/thinkabouthub/Configuration.EntityFramework/wiki
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            var setting1 = new ConfigurationSetting(new Model.ConfigurationSection("ConnectionStrings"), "connectionstring");
+            setting1.SetValue("test", typeof(string));
+            var value1 = setting1.GetValue<string>();
+
+            
+
+            var setting2 = new ConfigurationSetting(new Model.ConfigurationSection("Setting"), "setting1");
+            setting2.SetValue(setting1, setting1.GetType());
+            var value2 = setting2.GetValue(setting1.GetType());
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
